431fdee951882dbba95f5c5ff5aa1d0912c8e9b1
Kat Scriver <klescriver@gmail.com>
2020-08-20 13:46:33 -0600

    Adds test for planet attributes.

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -6,6 +6,13 @@ class PlanetTest < MiniTest::Test
   def test_it_exists
     earth = Planet.new("Earth", 3)
 
-    assert_instance_of Planet, earth 
+    assert_instance_of Planet, earth
+  end
+
+  def test_it_has_attributes
+    earth = Planet.new("Earth", 3)
+
+    assert_equal "Earth", earth.name
+    assert_equal 3, earth.order
   end
 end
